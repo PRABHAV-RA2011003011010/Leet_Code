3dad4d89b0913101b13fcd7954c5ab6095e681a0
PRABHAV-RA2011003011010 <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2024-08-23 10:18:39 +0530

    Time: 590 ms (26.41%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select employee_id,department_id from employee group by employee_id having count(*)=1
 union
-select employee_id,department_id from employee where primary_flag='Y' group by employee_id 
+select employee_id,department_id from employee where primary_flag='Y' 
