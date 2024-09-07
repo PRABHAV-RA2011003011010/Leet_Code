1d40c1c1d45e1350a1cbc6c446228e2971b8ee3b
PRABHAV-RA2011003011010 <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2024-09-07 10:44:32 +0530

    Time: 61 ms (82.66%) | Memory: 17.3 MB (63.38%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if not root or not head:
+            return False
+          
+        def dfs(root):
+            if not root:
+                return False
+            if root.val==head.val and trav(root,head): 
+                return True
+            return dfs(root.left) or dfs(root.right)
+            
+            
+
+        def trav(node,cur):
+            if not node and cur:
+                return False
+            elif not node and not cur:
+                return True
+            elif not cur:
+                return True
+            
+            if node.val!=cur.val:
+                return False
+                
+            return trav(node.left,cur.next) or trav(node.right,cur.next)
+    
+        return dfs(root)
+        
+        
