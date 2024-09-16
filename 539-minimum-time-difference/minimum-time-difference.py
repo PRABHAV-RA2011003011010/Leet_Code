ccbc33e4ffe0585b272a2c16d3ea3c5e992e681a
PRABHAV-RA2011003011010 <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2024-09-16 20:03:20 +0530

    Time: 59 ms (94.88%) | Memory: 19.5 MB (79.74%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+
+        if len(timePoints)>60*24:
+            return 0
+
+        min_distance=60*24
+        minute=[]
+
+        for time in timePoints:
+
+            hours,minutes=map(int,time.split(':'))
+            minute.append(hours*60+minutes)
+            
+        minute.sort()
+
+        for i in range(len(minute)-1):
+            min_distance=min(min_distance,minute[i+1]-minute[i])
+        
+        min_distance=min(min_distance,24*60+minute[0]-minute[-1])
+
+        return min_distance
+        
+               
+            
+
+
+        
+
+
+        
