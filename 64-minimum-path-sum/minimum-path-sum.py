29b2c38379568a277827d8b9eb223e0699b8d16c
PRABHAV-RA2011003011010 <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2024-09-22 19:10:53 +0530

    Time: 119 ms (40.12%) | Memory: 18.4 MB (37.37%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -3,13 +3,7 @@ class Solution:
         
         rows=len(grid)
         cols=len(grid[0])
-        if rows==1:
-            return sum(grid[0])
-        if cols==1:
-            sum1=0
-            for x in grid:
-                sum1+=sum(x)
-            return sum1
+        
 
         for col in range(1,cols):
             grid[0][col]+=grid[0][col-1]
