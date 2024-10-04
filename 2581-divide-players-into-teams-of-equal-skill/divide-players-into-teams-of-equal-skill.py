158dbf3b6e9bb2d433acb02b8d2e235480033082
PRABHAV-RA2011003011010 <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2024-10-04 08:30:51 +0530

    Time: 441 ms (13.27%) | Memory: 30.5 MB (6.90%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,28 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+
+        n=len(skill)
+        pairs=int(n/2)
+        total_skills=sum(skill)
+        value_of_pair=total_skills/pairs
+        value=int(value_of_pair)
+        res=0
+        if value_of_pair!=value:
+            return -1
+
+        freq=defaultdict(int)
+        for num in skill:
+            freq[num]+=1
+
+        for num in skill:
+            if freq[value-num]>0:
+                res+=num*(value-num)
+                freq[value-num]-=1
+            else:
+                return -1
+        
+        print(value)
+        print(freq)
+        
+        return int(res/2)
+        
