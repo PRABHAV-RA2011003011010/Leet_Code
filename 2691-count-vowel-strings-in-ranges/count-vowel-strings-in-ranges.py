af91e159be4716d77a16c21b3777f4a7b929ea9d
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-01-08 09:51:48 +0530

    Time: 16 ms (75.90%) | Memory: 49.7 MB (48.76%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,13 +1,24 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        isVowel=1+(1<<(ord('e')-ord('a')))+(1<<(ord('i')-ord('a')))+(1<<(ord('o')-ord('a')))+(1<<(ord('u')-ord('a')))
-        n, qz=len(words), len(queries)
-        cnt=[0]*(n+1)
-        for i, w in enumerate(words):
-            cnt[i+1]=cnt[i]+(((1<<(ord(w[0])-ord('a')))& isVowel)!=0 and ((1<<(ord(w[-1])-ord('a')))& isVowel)!=0)
-        ans=[0]*qz
-        for i, (q0, q1) in enumerate(queries):
-            ans[i]=cnt[q1+1]-cnt[q0]
-        return ans
         
+        vowels={'a','e','i','o','u'}
+        n=len(words)
+        prefix=[0]*(n+1)
+        ans=[]
+        
+        for i in range(len(words)):
+            prefix[i+1]=prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                    prefix[i+1]+=1
+
+        
+
+        for start,end in queries:
+            ans.append(prefix[end+1]-prefix[start])
+            
+        return ans
+                    
+
+
+
         
