1ab69c1f5198b6b728c1246808aa33d8fbfa1bf4
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-08-13 22:31:33 +0530

    Time: 226 ms (22.61%), Space: 48.6 MB (6.42%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
@@ -1,18 +1,21 @@
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        mod = 10**9 + 7
 
-        bins, rep = [], 1
-        while n > 0:
-            if n % 2 == 1:
-                bins.append(rep)
-            n //= 2
-            rep *= 2
+        powers=[]
+        res=[]
+        modulo=10*9+7
+        
+        binary = format(n, 'b')
+        
+        for i in range(len(binary)):
+            if binary[len(binary)-i-1]=='1':
+                powers.append(2**i)
+        print(powers)
+        for query in queries:
+            product=1
+            for i in range(query[0],query[1]+1):
+                product = product*powers[i]% (10**9 + 7)
+            
+            res.append(product)
 
-        ans = []
-        for left, right in queries:
-            cur = 1
-            for i in range(left, right + 1):
-                cur = cur * bins[i] % mod
-            ans.append(cur)
-        return ans
+        return res
