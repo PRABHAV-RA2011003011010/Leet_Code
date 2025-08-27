2d02ef538dae7e7b89a593be9568eee0ad7d5bfe
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-08-27 19:09:13 +0530

    Time: 169 ms (34.28%), Space: 29.7 MB (91.64%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,24 +3,23 @@ class Solution:
         res=0
         rows=len(matrix)
         cols=len(matrix[0])
-        dp=[[0]*cols for _ in range(rows)]
+       
 
         for i in range(rows):
             if int(matrix[i][0])==1:
-                dp[i][0]=int(matrix[i][0])
-                if not res:
+                res=1
+                break
+        if not res:
+            for j in range(cols):
+                if int(matrix[0][j])==1:
                     res=1
-        for j in range(cols):
-            if int(matrix[0][j])==1:
-                dp[0][j]=int(matrix[0][j])
-                if not res:
-                    res=1
+                    break
         
         for i in range(1,rows):
             for j in range(1,cols):
                 if int(matrix[i][j])==1:
-                    dp[i][j]=1+min(int(dp[i-1][j]),int(dp[i][j-1]),int(dp[i-1][j-1]))
-                    res=max(res,dp[i][j])
+                    matrix[i][j]=1+min(int(matrix[i-1][j]),int(matrix[i][j-1]),int(matrix[i-1][j-1]))
+                    res=max(res,matrix[i][j])
         return res*res        
 
 
