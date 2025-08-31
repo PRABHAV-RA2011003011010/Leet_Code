64f1761559e3aa7321750d0ac4e4d48f1d1822b5
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-08-31 13:03:14 +0530

    Time: 11 ms (82.34%), Space: 20.6 MB (91.14%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        
+
+        dict=defaultdict(list)
+       
+
+        for word in strs:
+            key="".join(sorted(word))
+            dict[key].append(word)
+        return list(dict.values())
+           
+        
