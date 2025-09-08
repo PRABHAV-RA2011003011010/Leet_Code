62705f29c5ac26c774de4934e6465ed1a3c11deb
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-09-08 09:36:41 +0530

    Time: 0 ms (100%), Space: 17.5 MB (99.62%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -5,6 +5,7 @@ class Solution:
         while '0' in str(num2) or '0' in str(num1):
             num1+=1
             num2-=1
+            if '0' not in str(num2)+str(num1):
+                return [num1,num2]
         return [num1,num2]
-
         
