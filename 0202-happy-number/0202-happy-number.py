ce24950f092f911a3956d78790e4e6c495ea53db
PRABHAV_REDDY <126558346+PRABHAV-RA2011003011010@users.noreply.github.com>
2025-11-22 23:50:11 +0530

    Time: 0 ms (100%), Space: 17.7 MB (60.92%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-
-        while(n>=10):
-            
+        seen=set()
+        while n not in seen and n!=1:
+            seen.add(n)
             sum=0
             while(n!=0):
                 last_digit=n%10
                 sum+=last_digit**2
                 n=int(n/10)
             n=sum
-        return True if(n==1 or n==7) else False
+        return n==1
 
         
